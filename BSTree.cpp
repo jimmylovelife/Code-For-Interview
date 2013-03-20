24ba5d5959c1b52941126e6b0a2ccaceb2a4667f
U-CCR\ysun49 <ysun49@YSUN49-MOBL.ccr.corp.intel.com>
2013-03-20 21:11:02 +0800

    add BSTree struct

---
+++ BSTree.cpp
@@ -0,0 +1,69 @@
+/**
+¶ş·Ö²éÕÒÊ÷ 
+**/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Tree{
+        int data;
+        struct Tree *left;
+        struct Tree *right;
+} BSTree;
+
+BSTree* createBSTree() {
+     BSTree *root = (BSTree *) calloc(sizeof(BSTree), sizeof(char));
+     int tmp;
+     scanf("%d", &tmp);
+     root->data = tmp;
+     root->left = NULL;
+     root->right = NULL;
+     
+     while(scanf("%d", &tmp) != EOF) {
+         BSTree *ins = root;
+         while(1) {
+             if(ins->data >= tmp) {
+                 if(ins->left != NULL) {
+                     ins = ins->left;
+                     continue;
+                 } else {
+                     BSTree *newItem = (BSTree *) calloc(sizeof(BSTree), sizeof(char));
+                     newItem->data = tmp;
+                     newItem->left = NULL;
+                     newItem->right = NULL;
+                     ins->left = newItem;
+                     break;
+                 } 
+             } else {
+                 if(ins->right != NULL) {
+                     ins = ins->right;
+                     continue;
+                 }  else {
+                     BSTree *newItem = (BSTree *) calloc(sizeof(BSTree), sizeof(char));
+                     newItem->data = tmp;
+                     newItem->left = NULL;
+                     newItem->right = NULL;
+                     ins->right = newItem;
+                     break;
+                 }
+             }
+         }
+     }
+     return root;
+}
+
+void inorder(BSTree *bTree) {
+     if (bTree->left != NULL) {
+         inorder(bTree->left);
+     }
+     printf("%d\n", bTree->data);
+     if(bTree->right != NULL) {
+         inorder(bTree->right);
+     }
+     
+}
+
+int main() {
+    BSTree *root = createBSTree();
+    inorder(root);
+    system("pause");
+}
