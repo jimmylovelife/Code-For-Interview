31ba8a7d5ca5a62280ac43688383bf5f035c24dd
U-CCR\ysun49 <ysun49@YSUN49-MOBL.ccr.corp.intel.com>
2013-03-26 21:50:07 +0800

    add heap sort, merge sort

--- sort.cpp
+++ sort.cpp
@@ -2,6 +2,7 @@
 implement sort algorithm
 */
 #include <iostream>
+#include <malloc.h>
 using namespace std;
 //dumparray
 template<typename Tp>
@@ -116,6 +117,97 @@ void quicksort(Tp *array, int start, int end)
 
 }
 
+//πÈ≤¢≈≈–Ú
+template<typename Tp>
+void merge(Tp *array, int start, int end, int start1, int end1)
+{
+    Tp *tmp = (Tp *) malloc(sizeof(Tp)*(end1-start+1));
+    int pi=start,pj=start1, pk=0;
+    while(pi<=end && pj<=end1)
+    {
+        if(array[pi]<array[pj])
+        {
+            tmp[pk++] = array[pi++];
+        } else
+        {
+            tmp[pk++] = array[pj++];
+        }
+    }
+    for(int i=pi; i<=end; i++)
+    {
+        tmp[pk++] = array[i];
+    }
+    for(int i=pj; i<=end1; i++)
+    {
+        tmp[pk++] = array[i];
+    }
+    for(int i=0; i<pk; i++)
+    {
+        array[start++] = tmp[i];
+    }
+}
+template<typename Tp>
+void mergesort(Tp *array, int len)
+{
+    for(int i=1; i<len; i*=2)
+    {
+        for(int j=0; j<len; j+=2*i)
+        {
+            merge(array, j, j+i-1, j+i, j+2*i-1);
+        }
+    }
+}
+
+template <typename Tp>
+void swap(Tp *a, Tp *b)
+{
+    Tp t = *a;
+    *a = *b;
+    *b = t;
+}
+//Heap Sort
+template<typename Tp>
+void bigHeap(Tp *a, int start, int len)
+{
+    int max;
+    int temp = a[start];
+    int j = 2 * start + 1;
+    while(j<len)
+    {
+        if((j+1) < len && a[j+1]>=a[j])
+            j++;
+        if(a[j] <= temp)
+            break;
+        a[start] = a[j];
+        start = j;
+        j = 2*start+1;
+    }
+    a[start] = temp;
+}
+template<typename Tp>
+void buildBigHeap(Tp *array, int len)
+{
+   for(int i=len/2-1; i>=0; i--)
+   {
+       bigHeap(array, i, len);
+   }
+}
+template<typename Tp>
+void buildSmallHeap(Tp *array, int len)
+{
+
+}
+
+template<typename Tp>
+void heapSort(Tp *array, int len)
+{
+    buildBigHeap(array, len);
+    for(int i=len-1; i >=1; i--)
+    {
+        swap(&array[i], &array[0]);
+        bigHeap(array, 0, i);
+}
+}
 //TODO Other sort algorithm
 int main()
 {
@@ -123,8 +215,10 @@ int main()
     int array[] = {1,3,2,0,6,9,7,3,2};
     //bubble(array, 9);
     //select(array, 9);
-    insert(array, 9);
+    //insert(array, 9);
     //quicksort(array, 0, 8);
+    //mergesort(array, 9);
+    heapSort(array, 9);
     dump(array, 9);
     return 0;
 }
